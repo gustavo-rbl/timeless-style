99d80f073e79a9da727f7286f13639a19261cc99
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-11 16:26:36 -0500

    add company navigation

--- src/components/Layout/CompanyLayout.jsx
+++ src/components/Layout/CompanyLayout.jsx
@@ -1,8 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function CompanyLayout() {
   return (
     <>
+      <h2>Company</h2>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="." end>
+              About
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="contact">Contact</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="privacy-policy">Privacy Policy</NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Outlet />
     </>
   );
