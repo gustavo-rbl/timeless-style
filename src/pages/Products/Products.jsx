4a1b6a9dbb07e90dbe4ed7ce727f55bca43103a7
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-11 17:33:08 -0500

    render fetched products

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,35 @@
-import { useLoaderData } from "react-router-dom";
+import { nanoid } from "nanoid";
+import { Link, useLoaderData } from "react-router-dom";
+import { HandleDiscount } from "../../modules/HandleDiscount";
 
 function Products() {
   const products = useLoaderData();
 
-  return <div>Products</div>;
+  return (
+    <>
+      {products.map((product) => (
+        <div key={nanoid()}>
+          <p>{product.category.toUpperCase()}</p>
+
+          <img src={product.image} alt={product.title} />
+
+          <h2>
+            ${HandleDiscount(product.price)} <span>${product.price}</span>
+          </h2>
+
+          <h2>{product.title}</h2>
+
+          <p>
+            &#9733;{product.rating.rate}/5 ({product.rating.count})
+          </p>
+
+          <button>
+            <Link to={product.id.toString()}>View Details</Link>
+          </button>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Products;
