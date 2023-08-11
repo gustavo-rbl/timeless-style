e01ecd397f5d70eb5467cd34892f9e6d0f2d77fb
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-11 17:38:08 -0500

    render product details

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,22 @@
 import { useLoaderData } from "react-router-dom";
+import { HandleDiscount } from "../../modules/HandleDiscount";
 
 function Product() {
   const product = useLoaderData();
 
-  return <div>Product</div>;
+  return (
+    <>
+      <img src={product.image} alt={product.title} />
+      <h2>{product.title}</h2>
+      <p>
+        &#9733;{product.rating.rate}/5 ({product.rating.count})
+      </p>
+      <p>{product.description}</p>
+      <p>
+        ${HandleDiscount(product.price)} <span>${product.price}</span>
+      </p>
+    </>
+  );
 }
 
 export default Product;
