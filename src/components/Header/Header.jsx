655d3e80ed52a9f177f6a3a4d948548740aa201c
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-11 16:22:19 -0500

    add root navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h2>
+        <Link to=".">TimelessStyle</Link>
+      </h2>
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="products">Products</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="company">Company</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="cart">Cart</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
