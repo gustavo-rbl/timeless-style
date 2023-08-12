c6ac9a7b8b7fda99b8296d5fbb7e8e8c3a45ad74
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-12 11:47:33 -0500

    render Product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,20 +1,30 @@
 import { useLoaderData } from "react-router-dom";
 import { HandleDiscount } from "../../modules/HandleDiscount";
+import QuantityForm from "../../components/QuantityForm/QuantityForm";
 
 function Product() {
+  // fetched product
   const product = useLoaderData();
 
   return (
     <>
-      <img src={product.image} alt={product.title} />
-      <h2>{product.title}</h2>
-      <p>
-        &#9733;{product.rating.rate}/5 ({product.rating.count})
-      </p>
-      <p>{product.description}</p>
-      <p>
-        ${HandleDiscount(product.price)} <span>${product.price}</span>
-      </p>
+      <div>
+        <img src={product.image} alt={product.title} />
+
+        <h2>{product.title}</h2>
+
+        <p>
+          &#9733;{product.rating.rate}/5 ({product.rating.count})
+        </p>
+
+        <p>{product.description}</p>
+
+        <p>
+          ${HandleDiscount(product.price)} <span>${product.price}</span>
+        </p>
+      </div>
+
+      <QuantityForm product={product} />
     </>
   );
 }
