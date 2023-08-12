7eb9a8725dd8634a8cc51a56cca14044cc6f6858
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-12 16:44:46 -0500

    add Footer markup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,90 @@
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaSquareYoutube, FaTwitter } from "react-icons/fa6";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div>
+        <h3>Customer Service</h3>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="#">FAQs</Link>
+            </li>
+
+            <li>
+              <Link to="#">Order Lookup</Link>
+            </li>
+
+            <li>
+              <Link to="#">Returns</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <h3>Credit Card</h3>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="#">Pay</Link>
+            </li>
+
+            <li>
+              <Link to="#">Apply</Link>
+            </li>
+
+            <li>
+              <Link to="#">Benefits</Link>
+            </li>
+
+            <li>
+              <Link to="#">Gift Card</Link>
+            </li>
+
+            <li>
+              <Link to="#">Gift Card Balance</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <h3>Connect With us</h3>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="#">
+                <FaFacebookF />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="#">
+                <FaInstagram />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="#">
+                <FaSquareYoutube />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="#">
+                <FaTwitter />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
