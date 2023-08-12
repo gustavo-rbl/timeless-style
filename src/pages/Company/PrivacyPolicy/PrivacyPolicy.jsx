24301588f931abca7eb3ea36d9b317aef92d10e2
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-12 16:24:58 -0500

    add PrivacyPolicy markup

--- src/pages/Company/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/Company/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,5 +1,151 @@
 function PrivacyPolicy() {
-  return <div>PrivacyPolicy</div>;
+  return (
+    <>
+      <h2>Lorem, ipsum.</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus voluptatem suscipit,
+        nostrum perspiciatis delectus consectetur.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ipsa explicabo
+        molestiae harum tenetur atque aspernatur sapiente? Iste totam cupiditate tempore esse
+        reprehenderit beatae fugiat libero dolorem labore! Aliquam ipsa saepe, inventore cum quas,
+        temporibus quis nesciunt sunt excepturi obcaecati sequi, voluptate modi incidunt laboriosam.
+      </p>
+
+      <h3>Lorem ipsum dolor sit, amet consectetur adipisicing.</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ad aperiam, impedit
+        recusandae libero sapiente beatae quos quaerat sunt numquam dolorem in officiis, qui nobis
+        atque quam corrupti id blanditiis aliquid optio, consequuntur quis? Maiores eos recusandae
+        laborum!
+      </p>
+
+      <ul>
+        <li>Lorem ipsum, dolor sit amet consectetur adipisicing.</li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore eveniet repudiandae
+          necessitatibus facilis veritatis.
+        </li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolor.</li>
+        <li>Lorem ipsum dolor sit.</li>
+      </ul>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium maiores repellendus
+        aliquam quae, suscipit iusto? Commodi libero distinctio dolorum aspernatur magni
+        perspiciatis molestias. Ad illum voluptatem repellat explicabo, modi quia.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corrupti quam quo amet odio,
+        perspiciatis sapiente facilis alias accusantium ut beatae similique accusamus eius aliquam
+        nulla nemo ipsa odit possimus.
+      </p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe?</li>
+
+        <li>Lorem ipsum dolor sit.</li>
+
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus facere nam ipsa nemo
+          doloremque.
+        </li>
+
+        <li>Lorem, ipsum dolor.</li>
+      </ul>
+
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae totam explicabo nemo
+        deleniti veritatis magni hic eum quibusdam qui veniam, reprehenderit, necessitatibus rerum
+        perspiciatis incidunt accusamus ipsam vero. Rerum modi ratione, cumque tempora fuga maiores
+        magni suscipit? Autem, sint cumque laudantium praesentium consequatur reprehenderit quis.
+        Quos similique repellendus quam quod illum cumque magnam quae vero?
+      </p>
+
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae nam cum.</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique temporibus id autem
+        accusantium illo! Error aliquam dolor quo id perferendis voluptate expedita, impedit facilis
+        beatae repudiandae odit itaque unde! Beatae, eveniet corporis? Ex distinctio soluta labore?
+        Nihil ipsa quaerat quos suscipit! Veniam labore accusantium ducimus, beatae asperiores
+        deserunt rerum. Saepe natus deserunt odit facilis nam?
+      </p>
+
+      <h3>Lorem, ipsum.</h3>
+
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quae!</p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius magnam suscipit rem ullam
+        dolorum et laborum fugiat quibusdam aliquid fugit? In eveniet, quasi a, pariatur beatae
+        reiciendis voluptatibus quibusdam tenetur odit quae quis animi perspiciatis minima dolorum
+        libero quod! Dignissimos, omnis aspernatur inventore, ipsum eos blanditiis voluptatem
+        debitis, quibusdam asperiores voluptatibus quas voluptas accusamus minus quisquam amet
+        reiciendis recusandae molestias eveniet placeat pariatur. Temporibus architecto corporis
+        dolore repellendus, nostrum possimus voluptatibus vitae veritatis doloremque. Nihil nam,
+        officiis esse minima vitae quam. Blanditiis aspernatur iure vero, expedita nihil aliquam
+        optio dolores necessitatibus cupiditate quibusdam nobis quos nostrum ut unde nesciunt
+        consectetur.
+      </p>
+
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi!</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam amet quisquam, laborum quam
+        minus vero!
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi veritatis accusamus fugiat
+        iusto distinctio soluta eligendi tenetur eius quaerat maxime! Natus, consectetur. Non
+        molestiae excepturi hic voluptatum consectetur laborum sequi!
+      </p>
+
+      <ul>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore eveniet repudiandae
+          necessitatibus facilis veritatis.
+        </li>
+
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus facere nam ipsa nemo
+          doloremque.
+        </li>
+
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo id, officia atque nulla
+          architecto accusamus saepe, voluptate amet neque et ipsa error soluta ullam fugit!
+        </li>
+
+        <li>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo id, officia atque nulla
+          architecto accusamus saepe, voluptate amet neque et ipsa error soluta ullam fugit!
+        </li>
+      </ul>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam deleniti ratione eius
+        rerum, repudiandae rem ab iste eligendi praesentium ullam cupiditate in aperiam quam quo
+        ducimus suscipit, dignissimos repellendus beatae.
+      </p>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae voluptas nulla illum
+        consequuntur id saepe unde cum, expedita odit dicta sit suscipit repellat eum omnis beatae,
+        placeat aliquid. Architecto, iure!
+      </p>
+    </>
+  );
 }
 
 export default PrivacyPolicy;
