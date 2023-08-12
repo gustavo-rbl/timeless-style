70aa8d7977f4e7453c71ac92458db71f2ebaf3f7
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-12 16:26:25 -0500

    add About markup

--- src/pages/Company/About/About.jsx
+++ src/pages/Company/About/About.jsx
@@ -1,5 +1,21 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, tempora iste quas
+        necessitatibus magnam obcaecati quo doloribus, veritatis numquam modi doloremque est culpa
+        totam tenetur?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facilis officia quam. Atque
+        iusto est quos corporis quo quasi quas, similique reprehenderit ut dolor repellendus
+        laudantium optio. Minima vitae nobis perferendis ullam iusto corrupti eligendi aspernatur?
+        Possimus tempore quia culpa.
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </>
+  );
 }
 
 export default About;
