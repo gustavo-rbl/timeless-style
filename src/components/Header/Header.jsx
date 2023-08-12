faf479303ddf3df4db31d89345fbce68eace4faf
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-12 16:26:04 -0500

    add Header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,60 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import IndexStyle from "../../css/Index.module.css";
+import style from "./Header.module.css";
+import { FaBars, FaCartShopping, FaX } from "react-icons/fa6";
 
 function Header() {
+  const [isToggle, setIsToggle] = useState(false);
+
+  // toggles mobile menu
+  const toggleMenu = () => {
+    setIsToggle((old) => !old);
+  };
+
+  // auto close mobile menu
+  const handleExpanded = (e) => {
+    if (e.target.tagName === "UL") return;
+
+    if (isToggle) setIsToggle(false);
+  };
+
   return (
-    <header>
-      <h2>
-        <Link to=".">TimelessStyle</Link>
+    <header className={style.header}>
+      <div className={style.hamburgerMenu} onClick={toggleMenu}>
+        {isToggle ? <FaX /> : <FaBars />}
+      </div>
+
+      <h2 className={style.hero} onClick={handleExpanded}>
+        <Link to="." className={IndexStyle.gradientText}>
+          <span className={IndexStyle.fontWeight400}>Timeless</span>Style
+        </Link>
       </h2>
 
-      <nav>
-        <ul>
+      <nav className={`${style.nav} ${isToggle ? style.navActive : null}`}>
+        <ul className={style.navLinks} onClick={handleExpanded}>
           <li>
-            <NavLink to="products">Products</NavLink>
+            <NavLink
+              to="products"
+              className={({ isActive }) => (isActive ? style.active : style.navLink)}
+            >
+              Products
+            </NavLink>
           </li>
 
           <li>
-            <NavLink to="company">Company</NavLink>
+            <NavLink
+              to="company"
+              className={({ isActive }) => (isActive ? style.active : style.navLink)}
+            >
+              Company
+            </NavLink>
           </li>
 
           <li>
-            <NavLink to="cart">Cart</NavLink>
+            <NavLink to="cart" className={style.cart}>
+              <FaCartShopping />
+            </NavLink>
           </li>
         </ul>
       </nav>
