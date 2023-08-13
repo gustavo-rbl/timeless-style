b350476293069827af55dfe0d15380a7c229001f
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-13 14:12:07 -0500

    add Cart style

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,40 +1,46 @@
 import useCartStore from "../../store/Store";
+import { Link } from "react-router-dom";
+import IndexStyle from "../../css/Index.module.css";
+import style from "./Cart.module.css";
+import { FaTrashCan } from "react-icons/fa6";
 
 function Cart() {
   const cart = useCartStore((store) => store.cart);
   const deleteFromCart = useCartStore((store) => store.deleteFromCart);
-  const clearCart = useCartStore((store) => store.clearCart);
   const getTotal = useCartStore((store) => store.getTotal);
 
   return (
     <>
       {cart.length === 0 ? (
-        <h2>Empty</h2>
+        <div>
+          <p>There&#39;s nothing in your shopping cart, but the possibilities are endless!</p>
+          <Link to=".." relative="path" className={IndexStyle.buttonV01}>
+            Start Shopping
+          </Link>
+        </div>
       ) : (
         <>
           {cart.map((order) => (
-            <div key={order.orderID}>
-              <img src={order.img} alt={order.item} />
+            <div key={order.orderID} className={style.cartItem}>
+              <img src={order.img} alt={order.item} className={style.cartImg} />
 
               <div>
-                <h2>{order.item}</h2>
+                <h2 className={style.title}>{order.item}</h2>
 
-                <div>
-                  <p>{order.itemQuantity}</p>
-
-                  <p>${order.total}</p>
+                <div className={style.summary}>
+                  <p>Quantity: {order.itemQuantity}</p>
+                  <p>Total: ${order.total.toFixed(2)}</p>
                 </div>
               </div>
 
-              <button onClick={() => deleteFromCart(order.orderID)}>X</button>
+              <FaTrashCan onClick={() => deleteFromCart(order.orderID)} className={style.delete} />
             </div>
           ))}
 
-          <button onClick={clearCart}>Clear cart</button>
-
-          <p>Total: ${getTotal()}</p>
-
-          <button>Checkout</button>
+          <div className={style.checkout}>
+            <p>Total: ${getTotal().toFixed(2)}</p>
+            <button className={IndexStyle.buttonV01}>Checkout</button>
+          </div>
         </>
       )}
     </>
