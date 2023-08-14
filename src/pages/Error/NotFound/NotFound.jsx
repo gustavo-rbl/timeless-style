c58174e54a7f70e5c164e45ccf59248043470221
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-08-14 15:07:11 -0500

    add NotFound style

--- src/pages/Error/NotFound/NotFound.jsx
+++ src/pages/Error/NotFound/NotFound.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import IndexStyle from "../../../css/Index.module.css";
+import errorImg from "../../../assets/img/errorImg.svg";
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h2>Uh oh...</h2>
+
+      <p>We are experiencing problems</p>
+
+      <p>Please try again later</p>
+
+      <img src={errorImg} alt="Error Image" />
+
+      <Link to="/" className={IndexStyle.buttonV01}>
+        Try Again
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
